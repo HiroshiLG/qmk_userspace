95f77c8bf50ce76a02f936fba62b7de267d938e3
HiroshiLG <jorge__regula@hotmail.com>
2025-05-13 12:23:10 -0400

    Numbers in the main layer, |, \ and @ in the "down" layer.

--- keyboards/sofle/keymaps/personal/layout.h
+++ keyboards/sofle/keymaps/personal/layout.h
@@ -2,7 +2,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
  * COLEMAK
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * | Esc  |   !  |   "  |   #  |   $  |   %  |                    |   &  |   /  |   |  |   \  |   @  |  '   |
+ * | Esc  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  |  '   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Caps |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |  ´   |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
@@ -15,11 +15,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------'           '------''---------------------------'
  */
 [_COLEMAK] = LAYOUT(
-  KC_ESC,  LSFT(KC_1),  LSFT(KC_2),  LSFT(KC_3), LSFT(KC_4), LSFT(KC_5),                        LSFT(KC_6), LSFT(KC_7), KC_GRV,  RALT(KC_MINS), RALT(KC_Q), KC_MINS,
-  KC_CAPS, KC_Q,        KC_W,        KC_E,       KC_R,       KC_T,                              KC_Y,       KC_U,       KC_I,    KC_O,          KC_P,       KC_LBRC,
-  KC_TAB,  KC_A,        KC_S,        KC_D,       KC_F,       KC_G,                              KC_H,       KC_J,       KC_K,    KC_L,          KC_SCLN,    KC_BSPC,
-  KC_LSFT, KC_Z,        KC_X,        KC_C,       KC_V,       KC_B,       KC_MUTE,     RGB_TOG,  KC_N,       KC_M,       KC_COMM, KC_DOT,        KC_SLSH,    KC_RSFT,
-                        KC_LCTL,     KC_LGUI,    KC_LALT,    KC_LOWER,   KC_SPC,      KC_ENT,   KC_RAISE,   KC_RALT,    KC_RGUI, KC_RCTL
+  KC_ESC,  KC_1,  KC_2,     KC_3,     KC_4,     KC_5,                              KC_6,      KC_7,     KC_8,     KC_9,    KC_0,     KC_MINS,
+  KC_CAPS, KC_Q,  KC_W,     KC_E,     KC_R,     KC_T,                              KC_Y,      KC_U,     KC_I,     KC_O,    KC_P,     KC_LBRC,
+  KC_TAB,  KC_A,  KC_S,     KC_D,     KC_F,     KC_G,                              KC_H,      KC_J,     KC_K,     KC_L,    KC_SCLN,  KC_BSPC,
+  KC_LSFT, KC_Z,  KC_X,     KC_C,     KC_V,     KC_B,       KC_MUTE,     RGB_TOG,  KC_N,      KC_M,     KC_COMM,  KC_DOT,  KC_SLSH,  KC_RSFT,
+                  KC_LCTL,  KC_LGUI,  KC_LALT,  KC_LOWER,   KC_SPC,      KC_ENT,   KC_RAISE,  KC_RALT,  KC_RGUI,  KC_RCTL
 ),
 
 /* LOWER
@@ -28,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+-------+------+------+------+------|                    |------+------+------+------+------+------|
  * |  F7  |  F8   |  F9  |  F10 |  F11 |  F12 |                    |   4  |   5  |   6  |   *  |   [  |   ]  |
  * |------+-------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |Colemak|Lower |Raise |      |      |-------.    ,-------|   1  |   2  |   3  |   -  |   {  |   }  |
+ * |   |  |   \   |   @  |      |      |      |-------.    ,-------|   1  |   2  |   3  |   -  |   {  |   }  |
  * |------+-------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
  * |      |  UNDO | COPY |  DEL | PASTE| CUT  |-------|    |-------|   =  |   0  |   .  |   +  |   <  |   >  |
  * `------------------------------------------/       /     \      \-----------------------------------------.
@@ -37,10 +37,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------.            `------.`---------------------------.
  */
 [_LOWER] = LAYOUT(
-  KC_F1,   KC_F2,      KC_F3,       KC_F4,     KC_F5,    KC_F6,                              KC_7,       KC_8,    KC_9,    KC_PSLS,   LSFT(KC_8),    LSFT(KC_9),
-  KC_F7,   KC_F8,      KC_F9,       KC_F10,    KC_F11,   KC_F12,                             KC_4,       KC_5,    KC_6,    KC_PAST,   LSFT(KC_QUOT), LSFT(KC_BSLS),
-  XXXXXXX, XXXXXXX,    XXXXXXX,     XXXXXXX,   XXXXXXX,  XXXXXXX,                            KC_1,       KC_2,    KC_3,    KC_PMNS,   KC_QUOT,       KC_BSLS,
-  KC_LSFT, KC_UNDO,    KC_COPY,     KC_DEL,    KC_PASTE, KC_CUT,    _______,       _______,  LSFT(KC_0), KC_0,    KC_PDOT, KC_PPLS,   KC_NUBS,       LSFT(KC_NUBS),
+  KC_F1,   KC_F2,          KC_F3,       KC_F4,     KC_F5,    KC_F6,                              KC_7,       KC_8,    KC_9,    KC_PSLS,   LSFT(KC_8),    LSFT(KC_9),
+  KC_F7,   KC_F8,          KC_F9,       KC_F10,    KC_F11,   KC_F12,                             KC_4,       KC_5,    KC_6,    KC_PAST,   LSFT(KC_QUOT), LSFT(KC_BSLS),
+  KC_GRV,  RALT(KC_MINS),  RALT(KC_Q),  XXXXXXX,   XXXXXXX,  XXXXXXX,                            KC_1,       KC_2,    KC_3,    KC_PMNS,   KC_QUOT,       KC_BSLS,
+  KC_LSFT, KC_UNDO,        KC_COPY,     KC_DEL,    KC_PASTE, KC_CUT,    _______,       _______,  LSFT(KC_0), KC_0,    KC_PDOT, KC_PPLS,   KC_NUBS,       LSFT(KC_NUBS),
                        _______,     _______,   _______,  _______,   _______,       _______,  _______,    _______, _______, _______
 ),
 
